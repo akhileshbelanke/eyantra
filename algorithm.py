52559cfd5d2f4055b416de4a5fa9623451209f2e
AkhileshBelanke <akhilesh.belanke@gmail.com>
2024-06-22 12:25:47 -0500

    [EYRC] WIP - There are errors, and breaks the project. Commiting this change to save the progress.

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,8 @@ class Algorithm():
         self.path = []
 
     def next_location_to_go(self, current_car):
+        pass
 
-if __name__ == "__main__":
-    print("Cannot run this file direclty. Execute gui.py")
+    if __name__ == "__main__":
+        print("Cannot run this file direclty. Execute gui.py")
 
